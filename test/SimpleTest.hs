8d0f1ad29091d3981eb910448d150681cac49d11
Adam Mitha <adam.mitha@gmail.com>
2023-02-26 16:02:35 -0800

    Iterate through test/bins directory and run all test programs

--- test/SimpleTest.hs
+++ test/SimpleTest.hs
@@ -1,39 +1,33 @@
 module Main where
 
 import Cpu
-import Data.List.Split
 import qualified Data.Binary.Get as B
 import qualified Data.ByteString.Lazy as DL
+import Data.List.Split
+import Data.Int
 import Memory
-import System.Exit
 import System.Directory
+import System.Exit
 
 main = do
-  testLoop
+  dirs <- listDirectory "test/bins"
+  testLoop dirs
 
-testLoop = do
-  let mem =
-        Memory.make
-          [ (00, 0xff010113), -- addi  sp,sp,-16
-            (04, 0x00112623), --  sw  ra,12(sp)
-            (08, 0x00812423), --  sw  s0,8(sp)
-            (12, 0x01010413), --  addi  s0,sp,16
-            (16, 0xfea42a23), --  sw  a0,-12(s0)
-            (20, 0xff442503), --  lw  a0,-12(s0)
-            (24, 0xff442503), -- (24, 0x02a50533) --  mul  a0,a0,a0
-            (28, 0x00c12083), --  lw  ra,12(sp)
-            (32, 0x00812403), --  lw  s0,8(sp)
-            (36, 0x01010113) --  addi  sp,sp,16
-            -- , (40, 0x00008067) --  ret
-          ]
-      cpu = initCpu
-  mem2 <- loadTest "test/bins/square.bin"
-  printExecution cpu mem2
+data Test = Test { dir :: String, expected :: Int32 }
+
+testLoop [] = do return ()
+testLoop (path : t) = do
+  let cpu = initCpu
+  mem <- loadTest $ "test/bins/" ++ path
+  printExecution cpu mem
+  testLoop t
 
 loadTest :: FilePath -> IO Memory
 loadTest test = do
   bin <- DL.readFile test
-  return (Memory.make (Prelude.zip [0 .. 24] (Prelude.map word32FromChars (chunkBs 4 bin))))
+  return (Memory.make (Prelude.zip (instructionNums [0 .. fromIntegral $ DL.length bin - 1]) (Prelude.map word32FromChars (chunkBs 4 bin))))
+
+instructionNums = filter (\x -> rem x 4 == 0)
 
 chunkBs n bs
   | DL.null bs = []
